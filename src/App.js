2bcfe3b2059d9800e030e7167ca5f1358e6e7fb1
cone011 <32248132+cone011@users.noreply.github.com>
2023-01-16 10:31:54 -0300

    commit
    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
+//Forma Funcional
+function App() {
+  const [tasks, SetTasks] = useState([
+    "Sacar la ropa",
+    "Hacer la cama",
+    "Leer un rato",
+  ]);
+  const [task, SetTask] = useState("");
+
+  const handlerChangeValue = (event) => {
+    SetTask(event.target.value);
+  };
+
+  const addNewValue = (event) => {
+    event.preventDefault();
+    SetTasks([...tasks, task]);
+    SetTask("");
+  };
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+        <form onSubmit={addNewValue}>
+          <input
+            type="text"
+            id="new-task"
+            value={task}
+            placeholder="Ingresa una tarea y oprime Enter"
+            onChange={handlerChangeValue}
+          />
+        </form>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="wrapper">
+//         <div className="list">
+//           <h3>Por hacer:</h3>
+//           <ul className="todo">
+//             <li>Sacar la ropa</li>
+//             <li>Hacer la cama</li>
+//             <li>Leer un rato</li>
+//           </ul>
+//            <form>
+//              <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+//            </form>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
